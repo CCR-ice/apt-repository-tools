1229a1aae3cfaee1693d0f669cfabb618aa6d14c
CCR-ice <64931368+CCR-ice@users.noreply.github.com>
2022-06-21 22:01:40 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
-mkdir -p /etc/apt-repo/programs &&
-mkdir -p /etc/apt-repo/tools &&
+mkdir -p /etc/apt-repo-tools/programs &&
+mkdir -p /etc/apt-repo-tools/tools &&
+mkdir -p /etc/apt-repo-tools/apt-repo/dists/stable/main/binary-amd64 &&
 echo 'The repo will be located in the "/etc/" directory.' &&
-echo '' > /etc/apt-repo/tools/addpkg.sh &&
-chmod +x /etc/apt-repo/tools/addpkg.sh
-#echo '' > /etc/apt-repo/tools/addrepo.sh &&
-echo 'alias addpkg="/etc/apt-repo/tools/addpkg.sh"' >> /etc/bash.bashrc &&
-#echo 'alias addrepo="/etc/apt-repo/tools/addrepo"' >> /etc/bash.bashrc &&
+echo '' > /etc/apt-repo-tools/tools/addpkg.sh &&
+chmod +x /etc/apt-repo-tools/tools/addpkg.sh
+#echo '' > /etc/apt-repo-tools/tools/addrepo.sh &&
+echo 'alias addpkg="/etc/apt-repo-tools/tools/addpkg.sh"' >> /etc/bash.bashrc &&
+#echo 'alias addrepo="/etc/apt-repo-tools/tools/addrepo"' >> /etc/bash.bashrc &&
 echo "Finished."
